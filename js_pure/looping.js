29842f316c839acfed7ca1a775467f9ce1373403
irvanster <tovamaulana098@gmail.com>
2021-11-09 15:33:52 +0700

    week 7 javascript

---
+++ js_pure/looping.js
@@ -0,0 +1,21 @@
+// const reverseTriangle = (row, col)=> {
+//     for (let index = 0; index <= row; index++) {
+//     }
+// }
+
+// reverseTriangle(50)
+
+i= 5 //let atau var default nya?
+var i
+
+console.log(i)
+// while (i < 10) {
+//     console.log("The number is " + i)
+//     i++;
+// }
+
+// do {
+//     console.log("The number is " + i)
+//     i++;
+//   }
+//   while (i < 10);
---
+++ js_pure/variable.js
@@ -0,0 +1,28 @@
+/** 
+ * format variable: namaVariable = camelCase
+ * variable:
+ *  - var
+ *  - let
+ *  - const
+*/
+
+payment_success ='true'
+
+let payment_success
+console.log(payment_success)
+
+// siswa = 'ferdi'
+// siswa2 ='ahah'
+// let siswa2 = 'ari'
+// const siswa3 = 'anto'
+// siswa3='rifai'
+// console.log(siswa3)
+
+// console.log(payment_success)
+// console.log(siswa2)
+
+
+// next:
+// -const 
+// - let
+// var
